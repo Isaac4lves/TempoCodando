11d6b1d674ac1ef34d3fa0b42188609dcdbc2f65
Isaac❗  (Com enxaqueca) <147288252+Isaac4lves@users.noreply.github.com>
2023-12-11 18:28:20 -0300

    New Commit

--- main.py
+++ main.py
@@ -3,44 +3,44 @@ import time
 import json
 from datetime import date
 
-global Aberto, Dados
-Aberto = False
-Dados = "Data.json"  
+global Dados
+Info = "Informações.json"  
 
-for processo in psutil.process_iter(['pid', 'name']):
-    
-    if processo.info['name'] == "Code.exe":
-        Aberto = True
+horas = 0
+minutos = 0
+segundos = 0
 
-        horas = 0
-        minutos = 0
+dataNumero = date.today().weekday()
+nomeSemana = ["Seg","Ter","Qua","Qui","Sex","Sab","Dom"]
+data = nomeSemana[dataNumero]
+
+
+while ( "Code.exe" in (i.name() for i in psutil.process_iter()) ):
+                
+    with open(Info, 'r') as arquivo:
+        arq = json.load(arquivo)
+        arq[f"{data}"] = (f"{horas}h {minutos}m {segundos}s")
+                    
+    with open(Info, "w") as f:
+        json.dump(arq, f)
+                    
+
+    time.sleep(1)
+    segundos += 1
+    print(f"\rAberto por {horas}h {minutos}m {segundos}s", end="")
+
+    if segundos == 60: # minutos
         segundos = 0
-        data = str( date.today() )
-
-        while Aberto:
-            
-            with open(Dados, 'r',encoding='utf-8') as arquivo:
-                dados = json.load(arquivo)    
-                dados["Tempo"] = (f"{horas}h {minutos}m {segundos}s")
-                dados["Data"] = (str(data))
-                    
-            with open(Dados, "w") as f:
-                json.dump(dados, f)
-                    
-            
-            if processo.info['name'] == "Code.exe": # verificar se o vscode tá aberto
-                time.sleep(1)
-                segundos += 1
-                print(f"\rAberto por {horas}h {minutos}m {segundos}s", end="")
-
-                if segundos == 60: # minutos
-                    segundos = 0
-                    minutos += 1
+        minutos += 1
                     
                 
-                if minutos == 60: # hora
-                    minutos = 0
-                    horas += 1
                 
-        break
+    if minutos == 60: # hora
+        minutos = 0
+        horas += 1
+        
+ 
+    
+
+
 
